2a4aa953eb8f5456bfce93115090d6dda535d453
Goran-Sacic <goran.sacic@gmail.com>
2024-01-11 15:38:07 +0100

    test3

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ let persons = [
   },
   {
     id: 5,
-    name: "pEro pErica",
+    name: "pEro pEricaa",
     number: "00000000",
   },
 ];
@@ -117,7 +117,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
